d89ced69c45e2f750213499e0941f5e29ae4d2fd
abhinav-chinta <abhinavmessi3@gmail.com>
2020-04-29 16:16:45 +0530

    english braille

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-cd Frontend
-dir=`pwd`
-echo "Building $dir..."
-yarn build
-cd ..
+#cd Frontend
+#dir=`pwd`
+#echo "Building $dir..."
+#yarn build
+#cd ..
 echo "Starting Server..."
-python3 -m webbrowser -t "localhost:5000"
-python3 backend.py
+python -m webbrowser -t "localhost:5000"
+python backend.py
 
