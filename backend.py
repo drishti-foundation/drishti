3f0934b987c41489140638349be766cf7c1578e0
anirudh171202 <anirudh.sathiya@gmail.com>
2020-05-05 23:45:19 +0530

    updated

--- backend.py
+++ backend.py
@@ -1,14 +1,15 @@
 import os, sys, time
+sys.path.append(os.path.abspath("BrailleTranslator"))
 import flask
 from flask import jsonify, request, render_template, send_from_directory
 from fpdf import FPDF
-
+from main_final import text_braille
 from janitor import janitor
 from pdfConverter import pdf_convert
 from textFormatter import formatText
 from nmt import nmt
 from map_encoding import get_braille
-
+import re
 path = os.path.abspath("static")
 app = flask.Flask(__name__, template_folder=path)
 
@@ -34,27 +35,31 @@ def get_pdf_translate():
     input_pdf = request.files["file"]
     lang = request.form["lang"]
 
+    english_text = pdf_convert("en")
     print("[LANG]:   ", lang)
 
     input_pdf.save("out.pdf")
+    
+    if lang=="en":
+        braille = text_braille(english_text)
+    elif lang=="hi":
+        text = ""
+        print("[LENGTH OF TEXT]", len(english_text))
+        if len(english_text) >= MAX_LENGTH:
+            c = 0
+            for i in range((len(english_text)//MAX_LENGTH)+1):
+                ceil = min(MAX_LENGTH*(i+1), len(english_text))
+                text += nmt(english_text[MAX_LENGTH*(i):ceil])
+        else:
+            text = nmt(english_text)
+
         
-    text = ""
-    english_text = pdf_convert("en")
-    print("[LENGTH OF TEXT]", len(english_text))
-    if len(english_text) >= MAX_LENGTH:
-        c = 0
-        for i in range((len(english_text)//MAX_LENGTH)+1):
-            ceil = min(MAX_LENGTH*(i+1), len(english_text))
-            text += nmt(english_text[MAX_LENGTH*(i):ceil])
+        hindi_text = text
+        braille = janitor(get_braille(hindi_text))
     else:
-        text = nmt(english_text)
-
-
-    hindi_text = text
-    braille = janitor(get_braille(hindi_text))
-
+        print("Language not found, please try again")
     output_pdf = FPDF()
-    output_pdf.add_font('font', '', 'font.ttf', uni=True)
+    output_pdf.add_font('font', '', os.path.abspath('font.ttf'), uni=True)
     output_pdf.set_font('font', '', 14)
     output_pdf.add_page()
 
@@ -62,14 +67,15 @@ def get_pdf_translate():
     output_pdf.ln(LINE_HEIGHT*2)
 
     for text in [braille[(i-1)*LINE_LENGTH:i*LINE_LENGTH] for i in range(1, round(len(braille)/LINE_LENGTH))]:
-        output_pdf.write(LINE_HEIGHT, text)
-    output_pdf.ln(LINE_HEIGHT)
+        output_pdf.write(LINE_HEIGHT, re.sub('\?*', '', text))
+        output_pdf.ln(LINE_HEIGHT)
 
     file_route = f"downloads/{time.time()}.pdf"
     output_pdf.output(file_route, 'F')
 
     return jsonify({"route": file_route})
 # !SECTION
+
 
 @app.route("/downloads/<pdf_name>")
 def get_pdf(pdf_name):
