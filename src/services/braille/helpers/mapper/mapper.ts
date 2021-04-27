52ba8025eea05b5a0492090c738317407aa09874
Lutetium-Vanadium <luv.s7000@gmail.com>
2021-04-27 19:30:00 +0530

    fix binToBraille

--- src/services/braille/helpers/mapper/mapper.ts
+++ src/services/braille/helpers/mapper/mapper.ts
@@ -11,23 +11,38 @@ const MAPPERS = {
 /*
  * Converts a binary representation of a braille character to unicode braille character
  */
-
 const binToBraille = (s: number | '?') => {
   if (s === '?') return '?';
+
   let braille = '';
 
-  const revBits = (n: number) =>
-    ((n & (1 << 5)) >> 5) |
-    ((n & (1 << 4)) >> 3) |
-    ((n & (1 << 3)) >> 1) |
-    ((n & (1 << 2)) << 1) |
-    ((n & (1 << 1)) << 3) |
-    ((n & (1 << 0)) << 5);
+  // Converts the braille number to braille
+  // The braille number is in the following format
+  //
+  //   1 4
+  //   2 5
+  //   3 6
+  //
+  // So for the braille character '⠫', the number is 0b110101
+  //
+  // - first it reverses the bits
+  //   eg: 0b110101 -> 0b101011
+  // - then it adds 0x2800 which is one less than code point of the first unicode character (⠁)
+  const getBrailleCharCode = (n: number) =>
+    0x2800 +
+    (((n & (1 << 5)) >> 5) |
+      ((n & (1 << 4)) >> 3) |
+      ((n & (1 << 3)) >> 1) |
+      ((n & (1 << 2)) << 1) |
+      ((n & (1 << 1)) << 3) |
+      ((n & (1 << 0)) << 5));
 
-  if (s > 1 << 5) {
-    braille += String.fromCharCode(revBits(s >> 6));
+  // Some characters map to 2 braille characters
+  if (s > 1 << 6) {
+    braille += String.fromCharCode(getBrailleCharCode(s >> 6));
   }
-  braille += String.fromCharCode(revBits(s));
+
+  braille += String.fromCharCode(getBrailleCharCode(s));
 
   return braille;
 };
